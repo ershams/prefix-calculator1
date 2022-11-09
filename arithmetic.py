a70e81e798d2ee4f18737e0292d6f9120cac3074
ershams <eusra.shams@gmail.com>
2022-11-09 14:31:28 -0500

    Completed all functions

--- arithmetic.py
+++ arithmetic.py
@@ -3,33 +3,44 @@
 
 def add(num1, num2):
     """Return the sum of num1 and num2."""
+    sum = (num1+num2)
 
-    return 10
+    return sum
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
+    remain = (num1 - num2)
+    return remain
 
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
-
+    mult = (num1 * num2)
+    return mult
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
-
+    div = (num1 / num2 )
+    return div
 
 def square(num1):
     """Return the square of num1."""
-
+    square = num1 ** 2
+    return square
 
 def cube(num1):
     """Return the cube of num1."""
+    cube = num1 ** 3
+    return cube
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    sq = pow(num1, num2)
+    return sq
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    modulus = num1 % num2
+    return modulus
--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 """CLI application for a prefix-notation calculator."""
 
-from arithmetic import *
+from arithmetic import * 
 
 
 while True:
